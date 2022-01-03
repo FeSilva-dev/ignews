a35e484aa3d5c15019db48f0e4a1ffdc27551cdd
FeSilva-dev <feliper.silva011@gmail.com>
2022-01-03 03:39:01 -0300

    start ignite blog challange

---
+++ src/pages/index.tsx
@@ -0,0 +1,61 @@
+import { GetStaticProps } from 'next';
+import styles from './home.module.scss';
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+// Client-side
+// Server-side - informações especificas por usuários
+// Static Site Generation - paginas estáticas, como um post de blog
+
+export default function Home({product}: HomeProps) {
+  return (
+    <>
+      <Head >
+        <title>ig.news - Home</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for {product.amount} month</span>
+          </p>
+
+          <SubscribeButton priceId={product.priceId}/>
+
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl" /> 
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JZ1GaBU9zBkvKJumTtcZrIZ");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-us', {
+      style: "currency",
+      currency: "USD"
+    }).format(price.unit_amount / 100),
+  };
+
+  return{
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+}
